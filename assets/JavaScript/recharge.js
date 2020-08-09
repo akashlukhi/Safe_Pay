15fb6a4e883788807176563e3466b17ead1cdbd8
akashlukhi123 <48802690+akashlukhi123@users.noreply.github.com>
2020-08-09 20:36:03 +0530

    a

--- assets/JavaScript/recharge.js
+++ assets/JavaScript/recharge.js
@@ -1,34 +1,39 @@
 function validate() {
 
-   
-    var phn = document.getElementById("phone").value;
-    var patt = /^\d{10}$/;
-    var chkphn=patt.test(phn);
-  
-    if(chkphn == true){
-      window.location.href = "index.html";
-    }
-    else {} 
+
+  var phn = document.getElementById("phone").value;
+  var patt = /^\d{10}$/;
+  var chkphn = patt.test(phn);
+
+  if (chkphn == true) {
+    window.location.href = "index.html";
   }
-  
-  function phonevalidate(){
-    var phn = document.getElementById("phone").value;
-    var patt = /^\d{10}$/;
-    var chkphn=patt.test(phn);
-  
-    if(chkphn == false)
-    {
-      var tag = document.createElement("p");
-      var text = document.createTextNode("Mobile Number must be 10 digit*" );
-      tag.style.fontSize="10pt";
-      tag.style.color="red";
-      tag.appendChild(text);
-      var element = document.getElementById("phone-p");
-      element.removeChild(element.childNodes[2]);
-      element.appendChild(tag);
-    }
-    else{
-      var element = document.getElementById("phone-p").innerHTML="<p></p>";
-    }
+  else { }
+}
+
+function phonevalidate() {
+  var phn = document.getElementById("phone").value;
+  var patt = /^\d{10}$/;
+  var chkphn = patt.test(phn);
+
+  // if(chkphn == false)
+  // {
+  //   var tag = document.createElement("p");
+  //   var text = document.createTextNode("Mobile Number must be 10 digit*" );
+  //   tag.style.fontSize="10pt";
+  //   tag.style.color="red";
+  //   tag.appendChild(text);
+  //   var element = document.getElementById("phone-p");
+  //   element.removeChild(element.childNodes[2]);
+  //   element.appendChild(tag);
+  // }
+  // else{
+  //   var element = document.getElementById("phone-p").innerHTML="<p></p>";
+  // }
+  if (chkphn == false) {
+    document.getElementById("phone-p").style.removeProperty("display");
   }
-  
+  else {
+    document.getElementById("phone-p").style.setProperty("display", "none");
+  }
+}
--- assets/JavaScript/signup.js
+++ assets/JavaScript/signup.js
@@ -10,17 +10,28 @@ function validate() {
     var chkphn = patt.test(phn);
     var pattern = /^\d{14}$/;
     var chkacc = pattern.test(acc);
+    console.log(chkphn);
+    console.log(chkacc);
+    console.log("pass:"+pass);
+    console.log("repass"+ repass);
+    console.log(String(pass).length);
     
     if ((chkphn == true) && (chkacc == true) && (pass == repass) && (String(pass).length >= 8)) {
-         window.location.href = "index.html";
+        console.log(chkphn);
+        console.log(chkacc);
+        console.log("pass:"+pass);
+        console.log("repass"+ repass);
+        console.log(String(pass).length);
+        return true;
     }
     else { 
-        console.log("provide correct informations");
+       console.log("Incorrect Credentials");
+       return false;
     }
 }
 
 function phonevalidate() {
-    var phn = document.getElementById("phone").value;
+    /*var phn = document.getElementById("phone").value;
     var patt = /^\d{10}$/;
     var chkphn = patt.test(phn);
 
@@ -36,14 +47,22 @@ function phonevalidate() {
     }
     else {
         var element = document.getElementById("phone-p").innerHTML = "<p></p>";
-    }
+    }*/
+    var phn = document.getElementById("phone").value;
+    var patt = /^\d{10}$/;
+    var chkphn = patt.test(phn);
+    if (chkphn == false) {
+        document.getElementById("phone-p").style.removeProperty("display");
+   }
+   else{
+       document.getElementById("phone-p").style.setProperty("display","none");
+   }
 }
 
 
 function passwordvalidate() {
-    var pass = document.getElementById("password").value;
-
-    if (String(pass).length < 8) {
+    
+   /* if (String(pass).length < 8) {
         var tag = document.createElement("p");
         var text = document.createTextNode("Password must be at least 8 digit*");
         tag.style.fontSize = "10pt";
@@ -54,13 +73,21 @@ function passwordvalidate() {
         element.appendChild(tag);
     }
     else {
-        var element = document.getElementById("pass-p").innerHTML = "<p></p>";
+        var element = document.getElementById("pass-p").innerHTML = "";
+    }*/
+    var pass = document.getElementById("password").value;
+    if (String(pass).length < 8) {
+         document.getElementById("pass-p").style.removeProperty("display");
     }
+    else{
+        document.getElementById("pass-p").style.setProperty("display","none");
+    }
+
 }
 
 
 function repasswordvalidate() {
-    var repass = document.getElementById("re-password").value;
+    /*var repass = document.getElementById("re-password").value;
     var pass = document.getElementById("password").value;
 
     if (pass != repass) {
@@ -75,12 +102,21 @@ function repasswordvalidate() {
     }
     else {
         var element = document.getElementById("repass-p").innerHTML = "<p></p>";
-    }
+    }*/
+    var repass = document.getElementById("re-password").value;
+    var pass = document.getElementById("password").value;
+
+    if (pass != repass) {
+        document.getElementById("repass-p").style.removeProperty("display");
+   }
+   else{
+       document.getElementById("repass-p").style.setProperty("display","none");
+   }
 }
 
 
 function accountvalidate() {
-    var acc = document.getElementById("account").value;
+    /*var acc = document.getElementById("account").value;
     var patt = /^\d{14}$/;
     var chkacc = patt.test(acc);
 
@@ -96,5 +132,14 @@ function accountvalidate() {
     }
     else {
         var element = document.getElementById("account-p").innerHTML = "<p></p>";
-    }
+    }*/
+    var acc = document.getElementById("account").value;
+    var patt = /^\d{14}$/;
+    var chkacc = patt.test(acc);
+    if (chkacc == false) {
+        document.getElementById("account-p").style.removeProperty("display");
+   }
+   else{
+       document.getElementById("account-p").style.setProperty("display","none");
+   }
 }
